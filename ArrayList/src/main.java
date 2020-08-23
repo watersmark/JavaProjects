d305ee599f6226b03102cb8dc10ab818f19eaacc
EVGENII <watersmark@mail.ru>
2020-08-23 23:36:54 +0300

    All function ArrayList

--- ArrayList/src/main.java
+++ ArrayList/src/main.java
@@ -5,7 +5,17 @@ public class main {
     public static void main(String[] args) {
 
         ArrayList<String> first = new ArrayList<String>();
+        ArrayList<String> second = new ArrayList<String>();
 
+        second.add("qq");
+        second.add("2");
+        second.add("d2");
+
+        first = second;
+
+        //ArrayList<Integer> copyOfSecondList = (ArrayList<Integer>) second.clone();
+
+        /*
         first.add("First");
         first.add("Second");
         first.add("Third");
@@ -14,6 +24,32 @@ public class main {
             System.out.println(elem);
         }
 
+        System.out.println(first.get(1));
+        System.out.println(first.add("44"));
+
+        System.out.println(first);
+        first.clear();
+        System.out.println(first);
+        */
+
+
+
+        /*
+        ArrayList<Integer> temp1 = new ArrayList<Integer>();
+        temp1.add(2);
+        temp1.remove(new Integer(2));
+        System.out.println(temp1);
+        */
+
+
+        System.out.println(first.contains("qq"));
+        System.out.println(first);
+        first.remove("qq");
+        System.out.println(first);
+
+        System.out.println(first.indexOf("2"));
+
+        System.out.println(first.isEmpty());
 
     }
 }
